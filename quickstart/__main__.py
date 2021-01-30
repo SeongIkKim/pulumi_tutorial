ba9ae4ae3e0df2f5059a4fc59f4549790f3393cc
SeongIkKim <kpic1638@gmail.com>
2021-01-31 04:10:10 +0900

    Bucket StaticFile Serving

--- quickstart/__main__.py
+++ quickstart/__main__.py
@@ -1,16 +1,23 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
 from pulumi_aws import s3
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+# 기존 코드에서 웹사이트 속성을 추가한다.
+bucket = s3.Bucket('my-bucket',
+    website=s3.BucketWebsiteArgs(
+        index_document="index.html",
+    ))
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
-
+# acl='public-read'
+# 어떤 유저든 사이트에 접속해 index 파일에 접근할 수 있도록 한다.
+# content_type='text/html'
+# 파일의 타입이 html임을 명시한다.
 bucketObject = s3.BucketObject(
     'index.html',
+    acl='public-read',
+    content_type='text/html',
     bucket=bucket,
     content=open('site/index.html').read(),
-    )
+)
+
+# 유저들이 접근할 수 있는 url 엔드포인트 export
+pulumi.export('bucket_endpoint', pulumi.Output.concat('http://', bucket.website_endpoint))
